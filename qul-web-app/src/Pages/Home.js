0e558eaf3083ec558cff92907a852b698b327576
colincockburn <colincockburn64@gmail.com>
2024-03-24 13:22:06 -0400

    satrted home page

--- qul-web-app/src/Pages/Home.js
+++ qul-web-app/src/Pages/Home.js
@@ -2,9 +2,26 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to our website!</p>
+    <div className="flex flex-col justify-around items-center h-screen">
+
+      {/* both are column on mobile and row on desktop */}
+      <div className="flex flex-col justify-center items-center h-2/3 ">
+        <div className='p-5 '>
+          <h1 className="text-2xl font-bold font-custom">We believe in transparency between you and your future landlord</h1>
+          <p className="text-lg">Anonymously rate and review landlords in Kingston</p>
+        </div>
+        <img src="/flag.png" alt="Queens Flag" className='w-20 mt-20' style={{height: "140px", width: "130px"}} />
+      </div>
+
+      <div className="flex flex-col h-1/3 w-full px-10">
+        <button className="flex justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mb-5 rounded-xl">
+          Post a Review
+        </button>
+        <button className="flex justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mb-5 px-4 rounded-xl">
+          Read Reviews
+        </button>
+      </div>
+
     </div>
   );
 }
