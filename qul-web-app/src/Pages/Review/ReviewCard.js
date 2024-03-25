c19e107521b0f3c9ee495cfa0ac347d2490bab78
colincockburn <113646606+colincockburn@users.noreply.github.com>
2024-03-25 17:09:25 -0400

    gave review a fixup

--- qul-web-app/src/Pages/Review/ReviewCard.js
+++ qul-web-app/src/Pages/Review/ReviewCard.js
@@ -15,18 +15,31 @@ function ReviewCard() {
   // Function to generate stars for ratings
   const generateStars = (rating) => {
     return Array.from({ length: 5 }, (_, index) => 
-      index < rating ? '★' : '☆'
-    ).join('');
+      <span style={index < rating ? {color: 'gold'} : {color: 'black'}}>
+        {index < rating ? '★' : '☆'}
+      </span>
+    );
+  };
+
+  const ReviewCard = ({ title, text }) => {
+    return (
+      <div className="relative p-4 w-full mx-auto bg-gray-200 rounded-xl shadow-md flex flex-col items-start space-y-4 border border-black">
+        <h3 className="absolute top-0 left-5 -translate-y-1/2 bg-gray-200 px-2 text-lg font-bold text-gray-800">
+          {title}
+        </h3>
+        <p className="text-gray-600">{text}</p>
+      </div>
+    );
   };
 
   return (
-    <div className="max-w-md mx-auto bg-white rounded-lg shadow-md overflow-hidden m-6">
-      <div className="bg-red-600 text-center py-2 text-white text-xl font-bold">{reviewData.landlordName}</div>
-      <div className="p-4">
-        <div className="flex flex-col space-y-1">
+    <div className="flex flex-col items-center mx-auto bg-gray-200 rounded-lg shadow-md mb-6 w-full max-w-lg">
+      <div className="bg-red-600 rounded-lg text-center py-2 text-white text-xl font-bold w-full ">{reviewData.landlordName}</div>
+      <div className="p-6 w-full max-w-md">
+        <div className="flex flex-col space-y-2 mb-4">
           <div className="flex justify-between">
-            <span className="text-gray-700">Rent:</span>
-            <span className="text-left">{reviewData.rent}</span>
+            <span className="text-gray-700 ">Rent:</span>
+            <span className="text-left ">{reviewData.rent}</span>
           </div>
           <div className="flex justify-between">
             <span className="text-gray-700">Property Address:</span>
@@ -44,11 +57,8 @@ function ReviewCard() {
             <span className="text-gray-700">Recommendation:</span>
             <span className="text-left">{generateStars(reviewData.recommendation)}</span>
           </div>
-          <div className="mt-8 border border-qul-dark-gray rounded-lg p-2">
-            <p className="text-gray-700 font-bold mb-1">Review:</p>
-            <p className="text-gray-600 whitespace-pre-line">{reviewData.reviewText}</p>
-          </div>
         </div>
+        <ReviewCard  title='Review' text={reviewData.reviewText} />
       </div>
     </div>
   );
